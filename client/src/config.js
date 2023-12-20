eb489239faeb33db33fc9833097d84a32407ecb5
TanakaAkihiro0930 <resolution0930@gmail.com>
2023-12-20 19:42:13 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,6 @@
-export const API_URL =
-  "https://delta-sigma-ventures-server.vercel.app" || "http://localhost:5000";
+
+export const API_URL = window.location.hostname === "localhost" ? "http://localhost:5000" : "https://delta-sigma-ventures-server.vercel.app";
+console.log(
+    "API_URL :",
+    API_URL
+);
