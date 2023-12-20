1e5beb296270d332611275a17c1cc26c18500c34
TanakaAkihiro0930 <resolution0930@gmail.com>
2023-12-20 18:57:58 +0530

    Add server hosted URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+export const API_URL =
+  "https://delta-sigma-ventures-server.vercel.app" || "http://localhost:5000";
